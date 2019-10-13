bf2dcfc8ec70e4ca7e180f58ee1cd5f969075388
Chris Proctor <chris.proctor@gmail.com>
2019-10-13 02:41:39 -0400

    Adding lab 1 fucntional

---
+++ lab_01_functional.py
@@ -0,0 +1,50 @@
+# Unit 1 Lesson 1
+# Author: Your name
+
+# Each of these functions has a multi-line string (starts and ends with """) called a 
+# docstring, which describes what it does and gives an example of how it should work.
+# You should just leave the docstrings alone--your job is to replace the incorrect 
+# return value with a correct return value.
+
+def area_of_square(side_length):
+    """
+    Returns the area of a square with `side_length`.
+
+        >>> area_of_square(3)
+        9
+        >>> area_of_square(10)
+        100
+    """
+    return 0
+
+def area_of_rectangle(length, width):
+    """
+    Returns the area of a rectangle with `length` and `width`.
+
+        >>> area_of_rectangle(1, 2)
+        2
+        area_of_rectangle(5, 7)
+        35
+    """
+    return 0
+
+def area_of_triangle(base, height):
+    """
+    Returns the area of a triangle with `base` and `height`.
+
+        >>> area_of_triangle(2, 4)
+        4
+    """
+    return 0
+
+def can_build_rectangle(side_lengths, area):
+    """
+    Returns True if any combination of the side lengths 
+    can be used to make a rectangle with `area`.
+
+        >>> can_build_rectangle([3, 5, 7], 21)
+        True
+        >>> can_build_rectangle([2, 4, 6, 8], 20)
+        False
+    """
+    return False
